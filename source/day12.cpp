0ad28e2fcf7cb0386f3219e67912ae90deda7957
Kernivel <hugorevois@gmail.com>
2022-12-12 14:38:08 +0100

    Cleared a bit day 12

--- source/day12.cpp
+++ source/day12.cpp
@@ -13,7 +13,7 @@ pair<int,int> getStartPos(vector<vector<char>> grid);
 vector<pair<int,int>> getStartPoss(vector<vector<char>> grid);
 pair<int,int> getEndPos(vector<vector<char>> grid);
 
-class Part1
+class Djisktra
 {
     public:
     vector<vector<char>> grid;
@@ -22,14 +22,14 @@ class Part1
     pair<int,int> endPos;
     map<pair<int,int>,int> toVisit;
 
-    Part1(string path)
+    Djisktra(string path)
     {
         this->grid = getGrid(path);
         this->startPos = getStartPos(grid);
         this->endPos = getEndPos(grid);
     }
 
-    Part1(vector<vector<char>> _grid,pair<int,int> _startPos,pair<int,int> _endPos)
+    Djisktra(vector<vector<char>> _grid,pair<int,int> _startPos,pair<int,int> _endPos)
     {
         this->grid = _grid;
         this->startPos = _startPos;
@@ -46,8 +46,7 @@ class Part1
 
     bool canGoThere(int startI,int startJ,int targetI,int targetJ)
     {
-        //cout<<"checking if can go from "<<start<<" to "<<target<<"\n";
-        //cout << (int(tolower(target))<=int(tolower(start))+1)<<"\n";
+
         if(targetI<0 || targetI>=this->grid.size() || targetJ<0 || targetJ>this->grid[0].size())
         {
             return false;
@@ -62,11 +61,9 @@ class Part1
 
     void exploreD(const int i,const int j,int pathLength)
     {
-        //cout << "At : ["<<i<<"]["<<j<<"] = "<<this->grid[i][j] << " Pathlength is " << pathLength <<"\n";
         this->dpMap[(make_pair(i,j))] = pathLength;
         if(canGoThere(i,j,i+1,j) && this->dpMap.find(make_pair(i+1,j)) == this->dpMap.end())
         {
-            //cout<<"Checking down\n";
             if(this->toVisit.find(make_pair(i+1,j)) == this->toVisit.end())
             {
                 this->toVisit[make_pair(i+1,j)] = pathLength+1;
@@ -75,7 +72,6 @@ class Part1
 
         if(canGoThere(i,j,i-1,j) && this->dpMap.find(make_pair(i-1,j)) == this->dpMap.end())
         {
-            //cout<<"Checking up\n";
             if(this->toVisit.find(make_pair(i-1,j)) == this->toVisit.end())
             {
                 this->toVisit[make_pair(i-1,j)] = pathLength+1;
@@ -84,7 +80,6 @@ class Part1
 
         if(canGoThere(i,j,i,j+1) && this->dpMap.find(make_pair(i,j+1)) == this->dpMap.end())
         {
-            //cout<<"Checking right\n";
             if(this->toVisit.find(make_pair(i,j+1)) == this->toVisit.end())
             {
                 this->toVisit[make_pair(i,j+1)] = pathLength+1;
@@ -93,7 +88,6 @@ class Part1
 
         if(canGoThere(i,j,i,j-1) && this->dpMap.find(make_pair(i,j-1)) == this->dpMap.end())
         {
-            //cout<<"Checking left\n";
             if(this->toVisit.find(make_pair(i,j-1)) == this->toVisit.end())
             {
                 this->toVisit[make_pair(i,j-1)] = pathLength+1;
@@ -211,7 +205,7 @@ void printGrid(vector<vector<char>> grid)
 
 int main()
 {
-    Part1 res1 = Part1("./files/day12/input.txt");
+    Djisktra res1 = Djisktra("./files/day12/input.txt");
     res1.exploreD(res1.startPos.first,res1.startPos.second,0);
     cout <<"Res 1 is : "<< res1.dpMap[make_pair(res1.endPos.first,res1.endPos.second)] <<"\n";
     int bestRes = res1.dpMap[make_pair(res1.endPos.first,res1.endPos.second)];
@@ -219,22 +213,18 @@ int main()
     pair<int,int> endPos = getEndPos(res1.grid);
     for(unsigned int i = 0;i<startPoss.size();i++)
     {
-        Part1 resTemp = Part1(res1.grid,startPoss[i],endPos);
-        //cout << "Starting at : ["<<startPoss[i].first<<"]["<<startPoss[i].second<<"] = " << resTemp.grid[startPoss[i].first][startPoss[i].second]<<"\n";
+        Djisktra resTemp = Djisktra(res1.grid,startPoss[i],endPos);
         resTemp.exploreD(resTemp.startPos.first,resTemp.startPos.second,0);
         int possRes = resTemp.dpMap[make_pair(resTemp.endPos.first,resTemp.endPos.second)];
         if(possRes>0)
         {
-            cout<<"PossRes :" << possRes<<"\n";
             if(possRes<bestRes)
             {
-                cout << "New best res" <<possRes<<"\n";
                 bestRes = possRes;
             }
         }
     }
+
     cout <<"Res 2 is : "<< bestRes <<"\n";
-    /*int res2 = part1("./files/day11/input.txt",20,true);
-    cout <<"Res 2 is : "<< res2 <<"\n";*/
     return EXIT_SUCCESS;
 }
